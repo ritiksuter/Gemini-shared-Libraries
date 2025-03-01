92345b45c7861601e3bcb9fa46454c0310edade2
Ritik Suteri <141510124+ritiksuter@users.noreply.github.com>
2025-03-01 14:52:48 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,8 +1,9 @@
 def call(){
      echo "Here we need to Push the image to dockerHub"
-     withCredentials([usernamePassword(credentialsId: 'secretCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-            sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+     // withCredentials([usernamePassword(credentialsId: 'secretCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+            // sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+            sh "docker login -u ritiksuteri -p Rohit@123%"
             sh "docker push ritiksuteri/gemini-clone-app:v2"
-     }
+     // }
      echo "Docker image pushed successfully"
 }
